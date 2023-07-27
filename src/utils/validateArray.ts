3da8a2bd4173763950035e49ecf158a3b4de8bde
qoiqvi <111002830+qoiqvi@users.noreply.github.com>
2023-07-27 13:36:50 +0500

    validate fix

--- src/utils/validateArray.ts
+++ src/utils/validateArray.ts
@@ -3,37 +3,38 @@ import { TrainCharacteristics } from "../model/types/train"
 export const validateArrayValues = (arr: TrainCharacteristics[] | undefined, setIsValid: (arg: boolean) => void) => {
 	if (arr) {
 		let errorrArr: string[] = []
+
+		const validateSpeedInput = (speed: number) => {
+			const validSpeed = /^\d+$/
+			if (validSpeed.test(String(speed))) {
+				return setIsValid(true)
+			} else {
+				errorrArr.push("speedError")
+			}
+		}
+
+		const validateForceInput = (force: number) => {
+			const validForce = /^\d+(\.\d+)?$/
+			if (validForce.test(String(force))) {
+				return setIsValid(true)
+			} else {
+				errorrArr.push("forceError")
+			}
+		}
+		const validateAmperageInput = (engineAmperage: number) => {
+			const validAmperage = /^\d+$/
+			if (validAmperage.test(String(engineAmperage))) {
+				return setIsValid(true)
+			} else {
+				errorrArr.push("amperageError")
+			}
+		}
+
 		for (let i = 0; i < arr.length; i++) {
 			const { engineAmperage, force, speed } = arr[i]
-			const validateSpeedInput = () => {
-				const validSpeed = /^\d+$/
-				if (validSpeed.test(String(speed))) {
-					return setIsValid(true)
-				} else {
-					errorrArr.push("speedError")
-				}
-			}
-
-			const validateForceInput = () => {
-				const validForce = /^\d+(\.\d+)?$/
-				if (validForce.test(String(force))) {
-					return setIsValid(true)
-				} else {
-					errorrArr.push("forceError")
-				}
-			}
-
-			const validateAmperageInput = () => {
-				const validAmperage = /^\d+$/
-				if (validAmperage.test(String(engineAmperage))) {
-					return setIsValid(true)
-				} else {
-					errorrArr.push("amperageError")
-				}
-			}
-			validateSpeedInput()
-			validateForceInput()
-			validateAmperageInput()
+			validateSpeedInput(speed)
+			validateForceInput(force)
+			validateAmperageInput(engineAmperage)
 		}
 		return errorrArr
 	}
